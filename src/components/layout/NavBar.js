f5fd016448fed76b0a8479c24f7b122e02abe98c
Sara <saragonzalezburgin@hotmail.com>
2020-12-05 11:29:26 +0100

    Fix navbar bootstrap

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -7,10 +7,19 @@ export default class NavBar extends Component {
     return (
       <div>
         <div className="App">
-            <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed top" >
-            <a href="/" className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center">
-              <img src={logo} className="App-logo" alt="logo" />
-            </a>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed top">
+              <div className="container">
+                <a href="/" className="navbar-brand col-sm-3 col-md-2 mr-0">
+                <img src={logo} className="App-logo" alt="logo" />
+              </a>
+              <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item active">
+                  <a className="nav-link" href="/" style= {{fontSize: '20px', color: 'white'}}>Pókedex</a>
+                  </li>
+                </ul>
+              </div>
+              </div>
             </nav>
         </div>
       </div>
